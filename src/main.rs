ba398b1adbbf9b934454aa4faf38b1663da5f39b
Jimmy Sorting <jimmy.sorting@starrepublic.com>
2020-10-28 01:36:48 +0100

    progress

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,15 @@ use tui::layout::{Layout, Constraint, Direction};
 
 fn main() {
     let mut server = Server::bind("178.236.67.44:27015");
-    match server
-        .get_server_info() {
-            Response::Error(err) => panic!("{}", err),
-            Response::Ok(server_info) => {
-                println!("Server name: {}", server_info.name);
-                println!("Players: {}/{}", server_info.players, server_info.max_players);
-                println!("Map: {}", server_info.map);
-                println!("Server Type: {:?}", server_info.server_type);
-                println!("Environment: {:?}", server_info.environment);
-                println!("Visibility: {:?}", server_info.server_visibility);
-            }
-        }    
+    match server.get_server_info() {
+        Response::Error(err) => panic!("{}", err),
+        Response::Ok(server_info) => {
+            println!("Server name: {}", server_info.name);
+            println!("Players: {}/{}", server_info.players, server_info.max_players);
+            println!("Map: {}", server_info.map);
+            println!("Server Type: {:?}", server_info.server_type);
+            println!("Environment: {:?}", server_info.environment);
+            println!("Visibility: {:?}", server_info.server_visibility);
+        }
+    }
 }
